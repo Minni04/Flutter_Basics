f3b2d88bed92bc733d1d20108073283ba0436fc1
“Minni” <minnib04@gmail.com>
2024-02-13 08:03:57 +0530

    Stack Widget

---
+++ lib/StackWidget.dart
@@ -0,0 +1,40 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(FlutterApp()) ;
+}
+class FlutterApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My Flutter App",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.amber),
+      home: Myhomescreen(),
+    );
+  }
+}
+class Myhomescreen extends StatefulWidget {  @override
+State<StatefulWidget> createState() {
+  return  Myhomestate();}}
+class Myhomestate extends State<StatefulWidget> {
+  void press (){
+    print("Clicked") ;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("dashboard"),
+        backgroundColor: CupertinoColors.systemGreen,
+      ),
+      body: Container(
+        height: 300, width: 300,
+        child: Stack(children:[ Container(height: 200, width: 200, color: Colors.greenAccent,) ,
+          Positioned( top: 10 ,left: 10,child: Container(height: 200,width: 200,color: Colors.pinkAccent,)) ] ),
+      ),
+    );
+  }}
